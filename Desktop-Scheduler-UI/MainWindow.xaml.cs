d495b10d311a4ce73a90282a3965c78aae3123b3
dbzfanatic <dbzfanatic@gmail.com>
2022-05-24 06:30:57 +0000

    Localization for login window

--- Desktop-Scheduler-UI/MainWindow.xaml.cs
+++ Desktop-Scheduler-UI/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +26,18 @@ namespace Desktop_Scheduler_UI
         public MainWindow()
         {
             InitializeComponent();
+            CultureInfo curCult = CultureInfo.CurrentCulture;
+            if (curCult.ToString() != "en-US")
+            {
+                Assembly language = Assembly.LoadFrom(".\\" + curCult.ToString() + "\\Desktop-Scheduler-UI.resources.dll");
+                ResourceManager rm = new ResourceManager(String.Format("Desktop_Scheduler_UI.Properties.Resources.{0}",curCult.ToString()), language);
+                lblPass.Content = rm.GetString("loginString");
+                lblUser.Content = rm.GetString("userString");
+                txtWelcome.Text = rm.GetString("welcomeString");
+                btnExit.Content = rm.GetString("exitString");
+                btnLogin.Content = rm.GetString("loginString");
+
+            }
         }
 
         private void btnExit_Click(object sender, RoutedEventArgs e)
