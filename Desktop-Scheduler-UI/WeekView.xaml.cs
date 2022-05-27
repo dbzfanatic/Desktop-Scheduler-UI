8dc2510a781906138d1b0a52d5af5a21eaa40f92
dbzfanatic <dbzfanatic@gmail.com>
2022-05-27 03:17:44 +0000

    Working weekview

--- Desktop-Scheduler-UI/WeekView.xaml.cs
+++ Desktop-Scheduler-UI/WeekView.xaml.cs
@@ -20,10 +20,35 @@ namespace Desktop_Scheduler_UI
     public partial class WeekView : Window
     {
         public WeekView(Week curWeek)
-        {
+        {            
             InitializeComponent();
+
+            String[] weekDays = new String[] { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            String[] stringSplit;
             curWeek.Thursday = curWeek.Thursday + curWeek.Thursday + curWeek.Thursday + curWeek.Thursday;
-            dataGrid.ItemsSource = new List<Week> { curWeek };
+            String[] days = curWeek.ToArray();
+            String[,] apptDays = new String[7,2];
+            List<String[]> apptData = new List<String[]>();
+            for(int i= 0; i < 7;i++)
+            {
+                if (int.TryParse(days[i], out int trash))
+                {
+                    stringSplit = new String[] { days[i], "" };
+                }
+                else if (days[i] != "")
+                {
+                    stringSplit = (days[i]).Split(null, 2);
+                }
+                else
+                {
+                    stringSplit = new String[] { "", "" };
+                }
+
+                apptDays[i,1] = stringSplit[1];
+                apptDays[i,0] = weekDays[i] + "\n" + stringSplit[0];
+                apptData.Add(new String[] { apptDays[i, 0] , apptDays[i,1]});
+            }
+            dataGrid.ItemsSource = apptData;
 
         }
 
@@ -31,8 +56,11 @@ namespace Desktop_Scheduler_UI
         {
             for(int i = 0; i < 7; i++)
             {
-                ((DataGridTextColumn)dataGrid.Columns[i]).FontSize = Width / 150;
-                Height = dataGrid.Height+25;
+                if (dataGrid.Columns.Count > 0)
+                {
+                    /*((DataGridTextColumn)dataGrid.Columns[i]).FontSize = Width / 150;
+                    Height = dataGrid.Height + 25;*/
+                }
             }
         }
 
